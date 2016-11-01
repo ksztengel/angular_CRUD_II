ab91787874f19abcf16655f40ef462a5489b9d53
Kristin Sztengel <kcsztengel@gmail.com>
2016-11-01 16:12:59 -0600

    getting data server side

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const knex = require('../db/knex');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+/* GET users listing. */
+router.get('/', (req, res, next) => {
+  knex('bucaneers')
+    .then(bucaneers =>{
+      //send data to client
+      res.json(bucaneers)
+    })
+
 });
 
 module.exports = router;
